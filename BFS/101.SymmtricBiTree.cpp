db540f4ba14edc73fd8362c85972857a560f9358
bear Water <526830652@qq.com>
2021-03-17 13:56:30 +0800

    2021.3.17

---
+++ BFS/101.SymmtricBiTree.cpp
@@ -0,0 +1,70 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	bool isSymmetric(TreeNode* root) {
+
+		if (root == nullptr) return true;
+		vector<int> first_val;
+		vector<int> sec_val;
+
+		deque<TreeNode*> queue;
+		queue.push_back(root);
+
+		while (queue.size())
+		{
+			auto p = queue[0];
+			queue.pop_front();
+			if (p == nullptr)
+			{
+				first_val.push_back(-1);
+				continue;
+			}
+			first_val.push_back(p->val);
+			// fun(p);
+
+
+			if (p->left) queue.push_back(p->left);
+			else queue.push_back(nullptr);
+
+			if (p->right) queue.push_back(p->right);
+			else queue.push_back(nullptr);
+		}
+
+		queue.push_back(root);
+
+		while (queue.size())
+		{
+			auto p = queue[0];
+			queue.pop_front();
+			if (p == nullptr)
+			{
+				sec_val.push_back(-1);
+				continue;
+			}
+			sec_val.push_back(p->val);
+			// fun(p);
+			if (p->right) queue.push_back(p->right);
+			else queue.push_back(nullptr);
+
+			if (p->left) queue.push_back(p->left);
+			else queue.push_back(nullptr);
+		}
+
+		// while()
+		return first_val == sec_val;
+
+
+
+
+	}
+};
--- README.md
+++ README.md
@@ -8,13 +8,13 @@ This project is created to record the problem and thought in leetcode.
 
 #### 155.Min Stack
 
-设置一个辅助栈专门用于放置当前最小的值，每次压栈时主栈压入新数据，辅助栈压入当前最小数据
+设置一个辅助栈专门用于放置当前最小的值，每次压栈时主栈压入新数据，辅助栈压入当前最小数据  
 
 
 
 #### 316.Remove `重要` 
 
-单调栈的问题，栈内元素大致递增，记录下串内字符的数量。有四题大致相同，总结一下规律
+单调栈的问题，栈内元素大致递增，记录下串内字符的数量。有四题大致相同，总结一下规律  
 
 
 
@@ -24,7 +24,9 @@ This project is created to record the problem and thought in leetcode.
 
 利用`ostream`将数字转换为字符串
 
-目前只知道怎么将istream初始化的利用，怎么重复使用还需要补充资料。
+目前只知道怎么将istream初始化的利用，怎么重复使用还需要补充资料。  
+
+
 
 
 
@@ -32,7 +34,9 @@ This project is created to record the problem and thought in leetcode.
 
 #### 703.KthlargestNumInDataStream
 
-无需对整个数组建堆，时间开销太大。建立一个K大的小顶堆，比堆顶小的全部都弃用。同时利用优先队列来建立小顶堆
+无需对整个数组建堆，时间开销太大。建立一个K大的小顶堆，比堆顶小的全部都弃用。同时利用优先队列来建立小顶堆  
+
+
 
 ### Sort
 
@@ -40,7 +44,13 @@ This project is created to record the problem and thought in leetcode.
 
 注意考虑整个情况，不要上来就写
 
-### Greedy 
+  
+
+
+
+### Greedy   
+
+
 
 ### Graphics
 
@@ -50,7 +60,17 @@ This project is created to record the problem and thought in leetcode.
 
 1.char 类型的字符串 ‘0’ -- ‘9’ 转数字 可以不用isstringstream，使用 letter - '0' 即可
 
-2.回归开始判断终止条件前，不要做任何操作。
+2.回归开始判断终止条件前，不要做任何操作。  
+
+
+
+### BFS 
+
+#### 101.对称二叉树
+
+自己的思路：利用两次BFS，第一次先进左子树、再进右子树。第二次先进右子树，再进左子树即可
+
+大佬的递归思路：左子树的左子树和右子树的右子树相同 && 左子树的右子树和右子树的左子树相同  
 
 
 
